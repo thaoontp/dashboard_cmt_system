b4302cf858e4d0799f6d65a6bffa6d1b9201cea3
MTHN912 <mthnguyen912na@gmail.com>
2024-06-13 15:46:59 +0700

    first commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,58 +1,43 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router';
 
-import AdminLayout from "../layouts/AdminLayout/AdminLayout.vue"
-import AuthLayout from "../layouts/AuthLayout/AuthLayout.vue"
+import AdminLayout from '../layouts/AdminLayout/AdminLayout.vue';
+import AuthLayout from '../layouts/AuthLayout/AuthLayout.vue';
 
 // router Admin
-import LoginAdmin from "../views/LoginAdmin/LoginAdmin.vue"
-import HomeAdmin from "../views/HomeAdmin/Home.vue"
+import HomeAdmin from '../views/HomeAdmin/Home.vue';
+import LoginAdmin from '../views/LoginAdmin/LoginAdmin.vue';
 
-import RegisterOrgan from "@/views/Organizations/RegisterOrgan/RegisterOrgan.vue"
-import HomeOrgan from "../views/Organizations/HomeOrgan/HomeOrgan.vue"
-import LoginOrgan from "../views/Organizations/LoginOrgan/LoginOrgan.vue"
+import RegisterOrganization from '../views/Organizations/RegisterOrgan/RegisterOrganization.vue';
 
 const routes = [
 	// router Admin
 	{
-		path: "/",
+		path: '/',
 		component: HomeAdmin,
 		meta: {
 			layout: AdminLayout,
-		}
+		},
 	},
 	{
-		path: "/user/login",
+		path: '/user/login',
 		component: LoginAdmin,
 		meta: {
 			layout: AuthLayout,
-		}
+		},
 	},
-	// router Organization
+	// Route Organization
 	{
-		path: "/organization",
-		component: HomeOrgan,
-		meta: {
-			layout: AdminLayout,
-		}
-	},
-	{
-		path: "/organization/register",
-		component: RegisterOrgan,
+		path: '/organization',
+		component: RegisterOrganization,
 		meta: {
 			layout: AuthLayout,
-		}
+		},
 	},
-	{
-		path: "/organization/login",
-		component: LoginOrgan,
-		meta: {
-			layout: AuthLayout,
-		}
-	},
-]
+];
+
 const router = createRouter({
 	history: createWebHistory(),
 	routes,
-})
+});
 
 export default router;
